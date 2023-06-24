2b99e467833333b1a44a6296bcee6027b73bf25a
Pablo Suazo <83501148+elefantesofisticado@users.noreply.github.com>
2023-06-23 23:23:09 -0400

    Update archivo.js

--- archivo.js
+++ archivo.js
@@ -1,3 +1,4 @@
-var mensajeElement = document.getElementById('sub-titulo2');
-
-mensajeElement.textContent = '¡Hola desde el script!';
+document.addEventListener('DOMContentLoaded', function() {
+      var mensajeElement = document.getElementById('sub-title2');
+      mensajeElement.textContent = '¡Hola desde el script!';
+    });
