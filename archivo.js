ff120458707e0446deb952addd8dcf3becb5799a
Pablo Suazo <83501148+elefantesofisticado@users.noreply.github.com>
2023-06-27 16:17:02 -0400

    Update archivo.js

--- archivo.js
+++ archivo.js
@@ -5,12 +5,6 @@ function saludar() {
   console.log("¡Hola desde el archivo JavaScript externo!");
 }
 
-// phytonshell
-const { PythonShell } = require('python-shell');
 
-PythonShell.run('pokeapi.py', null, function (err) {
-  if (err) throw err;
-  console.log('Módulo de Python ejecutado exitosamente.');
-});
 // Llamada a la función de saludo
 saludar();
