a7466ea4a33c94147e4a57ab92904763b6b7a63e
Pablo Suazo <83501148+elefantesofisticado@users.noreply.github.com>
2023-06-29 13:31:47 -0400

    Update pokeapi2.js

--- pokeapi2.js
+++ pokeapi2.js
@@ -1,9 +1,14 @@
 fetch('https://pokeapi.co/api/v2/pokemon/1')
   .then(function(response) {
-    if (response.ok) {
-      return response.json(); // Convertir la respuesta en formato JSON
+    if (response.status === 200) {
+      return response.json(); 
       const datosPokemon = response.data;
       const nombre = datosPokemon.name;
+      const urlImagen = datosPokemon.sprites.front_default;
+      const nombre = datosPokemon.name;
+      const id = datosPokemon.id;
+      const tipos = datosPokemon.types.map(tipo => tipo.type.name);
+
       console.log(nombre)
     }
     throw new Error('Error en la solicitud de la API: ' + response.status);
