dace8fd1d909477a03ecaad78661ea82a10650bd
Pablo Suazo <83501148+elefantesofisticado@users.noreply.github.com>
2023-06-30 19:16:30 -0400

    Update pokeapi2.js

--- pokeapi2.js
+++ pokeapi2.js
@@ -1,26 +1,44 @@
 console.log("start run script pokeapi2")
 
-const numeroAleatorio = Math.floor(Math.random() * 1010) + 1;
-const idPokemon = numeroAleatorio;
+function cambiarTitulo() {
+  var tituloElemento = document.getElementById("sub-title");
+  tituloElemento.textContent = "Reto: Acierta los typos de este Pokemon";
+}
+cambiarTitulo();
 
-fetch('https://pokeapi.co/api/v2/pokemon/${idPokemon}')
-  .then(function(response) {
-    if (response.ok) {
-      return response.json(); 
-    }
-    throw new Error('Error en la solicitud de la API: ' + response.status);
-  })
-  .then(function(data) {
-    document.addEventListener("DOMContentLoaded", function() {
-      console.log(data);
-      console.log(data.name);
-      console.log(data.types);
+function llamarAPI() {
+  fetch('https://pokeapi.co/api/v2/pokemon/254')
+    .then(function(response) {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Error al llamar a la API.');
+    })
+    .then(function(data) {
+      // Haz algo con los datos recibidos
+      const tipo = data.types;
+      var nombresTipos = tipo.map(function(tipo) {
+        return tipo.type.name;
+        });
+      console.log(nombresTipos);
+      console.log(tipo);
+      function nombrePokemon() {
+      var tituloElemento = document.getElementById("descripcion1");
+      tituloElemento.textContent = data.name + " " + nombresTipos;
+        }
+      nombrePokemon();
+    
+      var imagenURL = data.sprites.front_default;
+      var imagenPokemon = document.getElementById('imagenPokemon2');
+      imagenPokemon.src = imagenURL;
+      imagenPokemon.document.body.appendChild(imagenPokemon);
+    })
+    .catch(function(error) {
+      console.log(error);
     });
+}
 
-    // Hacer algo con los datos obtenidos
-  })
-  .catch(function(error) {
-    console.error('Error:', error);
-  });
+// Llama a la función para hacer la solicitud
+llamarAPI();
 
 console.log("end run script pokeapi2")
